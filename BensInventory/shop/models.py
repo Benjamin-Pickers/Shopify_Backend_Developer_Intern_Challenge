480e633dc8e80dbb2534733b64150403011fa418
Ben <ben10pickers@gmail.com>
2022-01-07 17:12:02 -0500

    Created Add product view and template

--- BensInventory/shop/models.py
+++ BensInventory/shop/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    productname = models.CharField(primary_key=True, max_length=40)
+    colour = models.CharField(max_length=20)
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    stock = models.IntegerField()
--- BensInventory/shop/views.py
+++ BensInventory/shop/views.py
@@ -3,6 +3,28 @@ from django.http import HttpResponse
 from django.urls import reverse, reverse_lazy
 from django.template import loader
 
+from .models import Product
+
 #Home Page
 def index(request):
     return render(request, 'shop/index.html')
+
+def AddProduct(request):
+
+    if request.method == 'POST':
+        form = request.POST
+
+        try:
+            product = Product.objects.get(pk=form['name'])
+            return render(request, 'shop/AddProduct.html', {'error_message':"Product already exists in inventory"})
+        except:
+            new_product = Product(productname=form['name'],
+                                  colour=form['colour'],
+                                  price=form['price'],
+                                  stock=form['stock']
+                                       )
+            new_product.save()
+            return render(request, 'shop/AddProduct.html', {'dataAcceptedMessage':"Product Successfully Added"})
+
+
+    return render(request, 'shop/addProduct.html')
