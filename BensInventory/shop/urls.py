71dee0e74d661f7fa95360167975a2655cf77dc7
Ben <ben10pickers@gmail.com>
2022-01-08 14:58:59 -0500

    Added UpdateProduct view and template

--- BensInventory/shop/urls.py
+++ BensInventory/shop/urls.py
@@ -6,4 +6,7 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.index, name='index'),
     path('AddProduct/', views.AddProduct, name='AddProduct'),
+    path('RemoveProduct/', views.RemoveProduct, name='RemoveProduct'),
+    path('FindProduct/', views.FindProduct, name='FindProduct'),
+    path('UpdateProduct/', views.UpdateProduct, name='UpdateProduct'),
 ]
--- BensInventory/shop/views.py
+++ BensInventory/shop/views.py
@@ -7,6 +7,12 @@ from .models import Product
 
 #Home Page
 def index(request):
+
+    all_products = Product.objects.all()
+
+    if all_products:
+        return render(request, 'shop/index.html', {'all_products':all_products})
+
     return render(request, 'shop/index.html')
 
 def AddProduct(request):
@@ -21,10 +27,55 @@ def AddProduct(request):
             new_product = Product(productname=form['name'],
                                   colour=form['colour'],
                                   price=form['price'],
-                                  stock=form['stock']
+                                  stock=int(form['stock'])
                                        )
             new_product.save()
             return render(request, 'shop/AddProduct.html', {'dataAcceptedMessage':"Product Successfully Added"})
 
 
     return render(request, 'shop/addProduct.html')
+
+def RemoveProduct(request):
+
+    if request.method == 'POST':
+        form = request.POST
+
+        try:
+            product = Product.objects.get(pk=form['name'])
+            product.delete()
+            return render(request, 'shop/removeProduct.html', {'dataAcceptedMessage':"Product Removed From Inventory"})
+        except:
+            return render(request, 'shop/removeProduct.html', {'error_message':"Product Could Not Be Removed Because It Does Not Exist"})
+
+
+    return render(request, 'shop/removeProduct.html')
+
+def FindProduct(request):
+
+    if request.method == 'POST':
+        form = request.POST
+
+        try:
+            product = Product.objects.get(pk=form['name'])
+            return render(request, 'shop/updateProduct.html', {'product':product})
+        except:
+            return render(request, 'shop/updateProduct.html', {'error_message':"Product Cannot be Updated Because It Does Not Exist"})
+    return render(request, 'shop/updateProduct.html')
+
+def UpdateProduct(request):
+
+    if request.method == 'POST':
+        form = request.POST
+
+        try:
+            product = Product(productname=form['name'],
+                              colour=form['colour'],
+                              price=form['price'],
+                              stock=int(form['stock'])
+                             )
+            product.save()
+            return render(request, 'shop/updateProduct.html', {'dataAcceptedMessage':"Product Successfully Updated"})
+        except:
+            return render(request, 'shop/updateProduct.html', {'error_message':"Product Could Not be Updated"})
+
+    return render(request, 'shop/updateProduct.html')
